70a11c73d378d13a9190a391bcbb5af10380db1f
NicoleSG <nicolesantamariagranizo@gmail.com>
2024-01-13 17:21:43 +0000

    added function to display the weather in the html with city name, temperature, wind and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,23 @@ function clearSearch() {
     localStorage.removeItem('location');
     searchHistory.empty();
 }
+//Function to display current weather
+function displayCurrentWeather(currentData) {
+    console.log(currentData);
+    currentWeather.html(''); //Clear any existing content within the element before adding new information.
+    //Get date and weather conditions icons
+    var currentDay = dayjs().format('DD/MM/YYYY');
+    var weatherIcon = currentData.weather[0];
+    console.log(currentDay);
+    //Display current weather information in HTML
+    currentWeather.append(`
+        <div class="mt-3 jumbotron jumbotron-fluid p-4">
+            <div class="container">
+                <h2 class="ml-2">${currentData.name} (${currentDay})<img class="ml-4" src="https://openweathermap.org/img/w/${weatherIcon.icon}.png" alt="${weatherIcon.description}"/></h2>
+                <p class="ml-2">Temp: ${Math.round(currentData.main.temp)} °C</p>
+                <p class="ml-2">Wind: ${Math.round(currentData.wind.speed)} M/S</p>
+                <p class="ml-2">Humidity: ${currentData.main.humidity}%</p>
+            </div>
+        </div>
+    `);
+}
